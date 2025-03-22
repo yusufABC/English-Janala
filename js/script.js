ae68c896fcdab6babd9688e80497c1955a805b4c
yusufABC <yusufabdullah20000@gmail.com>
2025-03-22 06:57:45 +0600

    added modal active button etc...'

--- js/script.js
+++ js/script.js
@@ -1,12 +1,76 @@
 
+const removeActive=()=>{
+    const activeButtons=document.getElementsByClassName("active")
+  for(let btn of activeButtons){
+    btn.classList.remove("active")
+  }
+ 
+}
+
+const addActive=(id)=>{
+    removeActive()
+    const clickButton=document.getElementById(`btn-${id}`)
+    if(clickButton){
+        clickButton.classList.add("active")
+    }
+}
+
+// modal showing 
+
+const loadCardDetails=async (id)=>{
+    const res= await fetch(`https://openapi.programming-hero.com/api/word/${id}`)
+    const res1=await res.json()
+    const index=res1.data
+
+
+    displayCardDeatils(index)
+
+    }
+
+    const displayCardDeatils=(details)=>{
+        console.log(details)
+        document.getElementById("card_details").showModal()
+        const detailsContainer=document.getElementById("details-container")
+        detailsContainer.innerHTML=`
+                <span class="text-xl">${details.word} (${details.meaning})</span>
+        <div>
+            <p class="font-bold">Meaning</p>
+            <p class="">${details.meaning}</p>
+        </div>
+      
+        <div>
+            <p class="font-bold">Example</p>
+            <p class="">${details.sentence}</p>
+        </div>
+        <div>
+            <p class="font-bold mb-2">সমার্থক শব্দ গুলো</p>
+            <div class="flex gap-3 ">
+                <button class="btn btn-xs bg-blue-100">${details.synonyms[0]}</button>
+                <button class="btn btn-xs bg-blue-100">${details.synonyms[1]}</button>
+                <button class="btn btn-xs bg-blue-100">${details.synonyms[2]}</button>
+              
+            </div>
+        </div>
+   
+        
+        `
+    }
+
+
+
+
+
+
 const loadVocBtn =async ()=>{
     let res=await fetch("https://openapi.programming-hero.com/api/levels/all")
     let res1=await res.json()
     // console.log(res1)
     let index=res1.data
     console.log(index)
+  
     displayVoc(index)
-    
+ 
+
 }
 
 
@@ -15,8 +79,11 @@ const loadVocCard =async()=>{
     let response=await res2.json()
     let index1=response.data
     console.log(index1)
+
     displayVocabsCards(index1)
 
+    addActive(id)
+    
 }
 
 const loadCategoryCard=(id)=>{
@@ -24,27 +91,21 @@ const loadCategoryCard=(id)=>{
     console.log(url)
     fetch(url)
     .then((res)=>res.json())
-    .then((data)=> displayVocabsCards(data.data))
-
+    .then((data)=> {
+        displayVocabsCards(data.data)
+    addActive(id)
+})
 }
 
 const displayVoc=(vocabulary)=>{
     const vocabularyContainer=document.getElementById("vocabulary-container")
-    console.log(vocabulary)
-    if (!Array.isArray(vocabulary) || vocabulary.length == 0) {
-        vocabularyContainer.innerHTML = `
-         <div class="col-span-full mx-auto flex justify-center flex-col items-center py-10 space-y-4 ">
-              <p class="text-xs">আপনি এখনো কোন Lesson Select করেন ন</p>
-            <h2 class="text-xl font-bold">একটি Lesson Select করুন।</h2>
-        </div>
-        `;
-        return;
-      }
+    
+
 
     vocabulary.forEach((voca)=>{
        const vocaDiv=document.createElement("div")
-    vocaDiv.innerHTML=` <button  onclick="loadCategoryCard(${voca.level_no})" class="btn btn-sm hover:text-white border-[#422AD5] text-[#422AD5]">
-                <img src="assets/fa-book-open.png" alt="Book Icon" class="inline-block mr-2"> Lesson-${voca.level_no}
+    vocaDiv.innerHTML=` <button id="btn-${voca.level_no}" onclick="loadCategoryCard(${voca.level_no})" class="btn btn-sm hover:text-white border-[#422AD5] text-[#422AD5]">
+                <img src="assets/fa-book-open.png" class="bg-white" alt="Book Icon" class="inline-block mr-2"> Lesson-${voca.level_no}
             </button>
         `;
 
@@ -56,6 +117,8 @@ const displayVoc=(vocabulary)=>{
 vocabularyContainer.append(vocaDiv)
         })
 }
+
+
 
 
 const displayVocabsCards=(vocabs)=>{
@@ -78,8 +141,12 @@ if (!Array.isArray(firstVocabs) || firstVocabs.length === 0) {
 }
 
     firstVocabs.forEach((vocab)=>{
+    
         console.log(vocab)
         const vocaCardDiv=document.createElement("div")
+    if(vocab.meaning==null){
+        vocab.meaning="অর্থ পাওয়া যাইনি"
+    }
         vocaCardDiv.innerHTML=`
         <div class="bg-base-100 shadow-sm my-6 mx-5  rounded">
                         <div class="card-body text-center ">
@@ -89,9 +156,14 @@ if (!Array.isArray(firstVocabs) || firstVocabs.length === 0) {
                           
                         </div>
                         <div class="flex justify-between p-4">
-                            <img src="https://img.icons8.com/?size=100&id=37212&format=png" class="w-5" alt="">
+                             <button onclick="loadCardDetails('${vocab.id}')" class="btn btn-xs">
+                             <img src="https://img.icons8.com/?size=100&id=37212&format=png" class="w-5" alt=""> 
+                             </button>  
+
         
-                            <img src="https://img.icons8.com/?size=100&id=y8FEyWnp5h1t&format=png&color=000000" class="w-5" alt="">
+                             <button class="btn btn-xs">
+                             <img src="https://img.icons8.com/?size=100&id=y8FEyWnp5h1t&format=png&color=000000" class="w-5" alt="">
+                              </button>
         
                         </div>
                        
@@ -107,4 +179,5 @@ if (!Array.isArray(firstVocabs) || firstVocabs.length === 0) {
 loadVocBtn()
 // loadVocCard()
 // displayVocabsCards()
-displayVoc
+// displayVoc()
+
